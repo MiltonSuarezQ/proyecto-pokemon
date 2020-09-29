cf01abbcaa67b4194d8aad71f86eca0e63bb09be
Jorge Barreto <barretoramirez@gmail.com>
2020-09-29 00:04:21 -0500

    Add pokemon-dos component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component'; 
+import { PokemonDosComponent } from './pokemon-dos/pokemon-dos.component'; 
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [{ path: '', component:PokemonComponent},
                         { path: 'pokemon', component:PokemonDetailComponent},
+                        { path: 'pokemon-dos', component:PokemonDosComponent},
                         { path: 'search', component:SearchComponent}];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import { PokemonDosComponent } from './pokemon-dos/pokemon-dos.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { SearchComponent } from './search/search.component';
 
@@ -11,6 +12,7 @@ import { SearchComponent } from './search/search.component';
   declarations: [
     AppComponent,
     PokemonDetailComponent,
+    PokemonDosComponent,
     PokemonComponent,
     SearchComponent
   ],
