5823b7bab4440b553dedc0f53109c6a3d97702c1
Jorge Barreto <barretoramirez@gmail.com>
2020-10-13 18:05:11 -0500

    Consumo de end-point

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonDetailComponent implements OnInit {
 
-  constructor() { }
+  public URL : any = 'https://pokeapi.co/api/v2/pokemon/';
+  public url : string;
+  public pokemonDetail : any;
+  public abilities: any;
+  public indices: any;
+  public moves: any;
+  public stats: any; 
+
+  constructor(private route: ActivatedRoute,private HttpClient:HttpClient) { }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.paramMap.get("id");
+    this.url = this.URL+id;
+    this.HttpClient.get(this.url).subscribe(
+      res=>{        
+        this.pokemonDetail = res;
+        this.abilities = this.pokemonDetail.abilities;
+        this.indices = this.pokemonDetail.game_indices;
+        this.moves = this.pokemonDetail.moves;
+        this.stats = this.pokemonDetail.stats;
+        console.log('Pokemon detail',this.pokemonDetail) 
+        console.log('Pokemon detail',this.indices)
+        console.log('Pokemon detail',this.moves)
+        console.log('Pokemon detail',this.stats)          
+      }, 
+      err=> console.log(err)
+    )
   }
 
 }
